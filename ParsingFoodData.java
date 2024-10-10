ed92da8225bd3a2e466236d10c2c551268a6f1f6
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-09 20:58:03 -0400

    set input to prompt for file name; created new fileInputStream assigned as file to be scanned; looped while scnr has next line, setting itemsArray as categories for each index separated by tabs; if conditional to read availability element to print only available items; closed scanner

--- ParsingFoodData.java
+++ ParsingFoodData.java
@@ -6,9 +6,27 @@ public class ParsingFoodData {
    public static void main(String[] args) throws IOException {
       Scanner scnr = new Scanner(System.in);
 
-      /* Type your code here. */
-    
+
+      System.out.println("Enter file name: ");
+      String fileName = scnr.nextLine();
+
+      FileInputStream fileStream = new FileInputStream(fileName);
+      Scanner fileScanner = new Scanner(fileStream);
+
+      while (fileScanner.hasNextLine()) {
+        String line = fileScanner.nextLine();
+        String[] itemsArray = line.split("\t");  
+        
+        String category = itemsArray[0];
+        String name = itemsArray[1];
+        String description = itemsArray[2];
+        String availability = itemsArray[3];  
+        
+        if (availability.equals("Available") || availability.equals("Not Available")) {
+           System.out.println(name + " (" + category + ") -- " + description);
+        }
+     }
+     fileScanner.close();
          }
       }
-   }
-}
+
